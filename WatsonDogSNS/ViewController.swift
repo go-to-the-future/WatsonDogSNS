c15579bca431d4e3dcc1d30896d7efd22d3d0a48
Kiyotan <appleumasugi.mjd@gmail.com>
2019-05-07 22:03:23 +0900

    Initial Commit

---
+++ WatsonDogSNS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatsonDogSNS
+//
+//  Created by Kiyoto Ryuman on 2019/05/07.
+//  Copyright © 2019 Kiyoto Ryuman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
